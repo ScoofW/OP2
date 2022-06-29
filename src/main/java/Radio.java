1313277d1ffbb62a52ecd4910b588eee80870e0c
ScoffW <stivie333@mai.ru>
2022-06-29 20:21:25 +0300

    First commit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,60 @@
+public class Radio {
+    private int numberStation;
+    private int volume;
+
+    public int getNumberStation() {
+        return numberStation;
+    }
+
+    public void setNumberStation(int numberStation) {
+        if (numberStation > 9) {
+            numberStation = 9;
+        }
+        if (numberStation < 0) {
+            numberStation = 0;
+        }
+        this.numberStation = numberStation;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        if (volume > 10) {
+            volume = 10;
+        }
+        if (volume < 0) {
+            volume = 0;
+        }
+        this.volume = volume;
+    }
+
+    public void upNumberStation() {
+        if (numberStation == 9) {
+            numberStation = 0;
+            return;
+        }
+        numberStation = numberStation + 1;
+    }
+
+    public void downNumberStation() {
+        if (numberStation == 0) {
+            numberStation = 9;
+            return;
+        }
+        numberStation = numberStation - 1;
+    }
+
+    public void upVolume() {
+        if (volume < 10) {
+            volume = volume + 1;
+        }
+    }
+
+    public void downVolume() {
+        if (volume > 0) {
+            volume = volume - 1;
+        }
+    }
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,120 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    public void  shouldUpVolume (){
+        Radio radio = new Radio();
+        radio.setVolume(9);
+        radio.upVolume();
+        assertEquals(10, radio.getVolume());
+    }
+
+    @Test
+    public void  shouldUpVolumeAboveRange (){
+        Radio radio = new Radio();
+        radio.setVolume(11);
+        radio.upVolume();
+        assertEquals(10, radio.getVolume());
+    }
+
+    @Test
+    public void  shouldUpVolumeBelowRange (){
+        Radio radio = new Radio();
+        radio.setVolume(-1);
+        radio.upVolume();
+        assertEquals(1, radio.getVolume());
+    }
+
+    @Test
+    public void  shouldDownVolumeAboveRange (){
+        Radio radio = new Radio();
+        radio.setVolume(11);
+        radio.downVolume();
+        assertEquals(9, radio.getVolume());
+
+    }
+
+    @Test
+    public void  shouldDownVolumeBelowRange (){
+        Radio radio = new Radio();
+        radio.setVolume(-1);
+        radio.downVolume();
+        assertEquals(0, radio.getVolume());
+
+    }
+
+    @Test
+    public void  shouldDownVolume (){
+        Radio radio = new Radio();
+        radio.setVolume(8);
+        radio.downVolume();
+        assertEquals(7, radio.getVolume());
+
+    }
+
+    @Test
+    public void shouldSetNumberStation(){
+        Radio radio = new Radio();
+        radio.setNumberStation(3);
+        assertEquals(3, radio.getNumberStation());
+
+    }
+
+    @Test
+    public void shouldSetNumberStationAboveRange(){
+        Radio radio = new Radio();
+        radio.setNumberStation(10);
+        assertEquals(9, radio.getNumberStation());
+
+    }
+
+    @Test
+    public void shouldSetNumberStationBelowRange(){
+        Radio radio = new Radio();
+        radio.setNumberStation(-1);
+        assertEquals(0, radio.getNumberStation());
+
+    }
+
+    @Test
+    public void  shouldUpNumberStation (){
+        Radio radio = new Radio();
+        radio.setNumberStation(5);
+        radio.upNumberStation();
+        assertEquals(6, radio.getNumberStation());
+
+    }
+
+    @Test
+    public void  shouldUpNumberStationIfMax (){
+        Radio radio = new Radio();
+        radio.setNumberStation(9);
+        radio.upNumberStation();
+        assertEquals(0, radio.getNumberStation());
+
+    }
+
+    @Test
+    public void  shouldDownNumberStation (){
+        Radio radio = new Radio();
+        radio.setNumberStation(5);
+        radio.downNumberStation();
+        assertEquals(4, radio.getNumberStation());
+
+    }
+
+    @Test
+    public void  shouldDownNumberStationIfMin(){
+        Radio radio = new Radio();
+        radio.setNumberStation(0);
+        radio.downNumberStation();
+        assertEquals(9, radio.getNumberStation());
+
+    }
+
+
+
+}
