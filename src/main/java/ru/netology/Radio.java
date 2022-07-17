a1175c03e9cc9300e93525b13c9d3d2e38b4d130
ScoffW <stivie333@mai.ru>
2022-07-17 15:01:05 +0300

    Added new requirements

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -7,28 +7,33 @@ public class Radio {
 
     private int currentRadioStationNumber;
     private int currentVolume;
-    private int minVolume = 0;
-    private int maxVolume = 100;
+    private final int minVolume = 0;
+    private final int maxVolume = 100;
 
-    public Radio(){
+    public Radio() {
     }
+
     public Radio(int radioCount) {
-        if (radioCount <= minRadioStationNumber){
+        if (radioCount <= minRadioStationNumber) {
             System.out.println("Нет возможности установить количество радиостанций меньше 1, количество останется без изменений, равным: " + getRadioCount());
             return;
         }
         this.radioCount = radioCount;
         this.maxRadioStationNumber = radioCount - 1;
     }
-    public int getRadioCount(){
+
+    public int getRadioCount() {
         return radioCount;
     }
+
     public int getMaxRadioStationNumber() {
         return maxRadioStationNumber;
     }
-    public int getCurrentRadioStationNumber(){
+
+    public int getCurrentRadioStationNumber() {
         return this.currentRadioStationNumber;
     }
+
     public void nextRadioStationNumber() {
         if (currentRadioStationNumber < maxRadioStationNumber) {
             currentRadioStationNumber++;
@@ -36,6 +41,7 @@ public class Radio {
             currentRadioStationNumber = minRadioStationNumber;
         }
     }
+
     public void prevRadioStationNumber() {
         if (currentRadioStationNumber > minRadioStationNumber) {
             currentRadioStationNumber--;
@@ -44,38 +50,33 @@ public class Radio {
         }
 
     }
+
     public void setRadioStationNumber(int radioStationNumber) {
         if (radioStationNumber >= minRadioStationNumber && radioStationNumber <= maxRadioStationNumber) {
             this.currentRadioStationNumber = radioStationNumber;
-        } else {
-            return;
         }
     }
+
     int getCurrentVolume() {
 
         return this.currentVolume;
     }
+
     public void increaseVolume() {
         if (currentVolume < maxVolume) {
             currentVolume++;
-        } else {
-            return;
         }
     }
+
     public void decreaseVolume() {
         if (currentVolume > minVolume) {
             currentVolume--;
-        } else {
-            return;
         }
     }
 
     public void setVolume(int currentVolume) {
         if (currentVolume >= minVolume && currentVolume <= maxVolume) {
             this.currentVolume = currentVolume;
-        } else {
-            return;
         }
-
     }
 }
